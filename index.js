74f1318ced32aa4b966de0c6893118b74171f3b4
Sezim <amangulovasezim@gmail.com>
2024-04-14 13:57:48 +0100

    interval is done with counter

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 
-
 // Task 1
-const stopID = setInterval((text) => {
-    console.log(text);
-}, 1000, 'Hello World')
 
-setInterval((a)=>  {
-        (clearInterval(stopID))
+let count = 0;
+const setInt = setInterval(() => {
+    console.log('Hello World');
+        count++;
+        if (count === 5){
         console.log('Done');
-}, 5000)
-
-
+        clearInterval(setInt);
+    }
+    }, 1000);
 
 
 //Task 2
-let func = () => {
-    setTimeout((four) => {
-       console.log(four);
-    }, 4000, 'Hello after 4 seconds');
-    setTimeout((eigth) => {
-        console.log(eigth);
-     }, 8000, 'Hello after 8 seconds')
-}
-func()
+let func = (a,b) => {
+    setTimeout((el) => {
+               console.log(el);
+},a,b)}
 
+func(4000, 'Hello after 4 seconds');
+func(8000, 'Hello after 8 seconds');
 
 
 
 //Task 3
-let day = +prompt('Введите число от 1 до 31')
+let day = +prompt('Введите число от 1 до 31');
 switch(true){
     case day>0 && day<=10: 
         console.log('первая декада');
@@ -47,12 +43,12 @@ switch(true){
 
 //задача 1 от Перизат
 let arr = ['t', 'e', 's', 't'];
-console.log(arr.slice(0,-3).concat(arr.slice(2,-1))) 
-console.log(arr.splice(0,2))
-console.log(arr)
+console.log(arr.slice(0,-3).concat(arr.slice(2,-1)));
+console.log(arr.splice(0,2));
+console.log(arr);
 
 
-//задача 2 от Перизат
+// //задача 2 от Перизат
 let arr2 = ['Я', 'изучаю', 'JavaScript'];
 arr2.splice(2, 0, 'сложный', 'язык');
 console.log(arr2);
